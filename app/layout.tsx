8156739c7d2243be7e83f540b6cb9a1977799bc9
Sohaib <mypesonel111@gmail.com>
2025-07-01 16:47:33 +0500

    Remove unused Navbar and Footer imports from layout component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { Navbar } from "@/components/Navbar";
-import { Footer } from "@/components/Footer";
 import { Toaster } from "react-hot-toast";
 import Providers from "../context/providers";
 
